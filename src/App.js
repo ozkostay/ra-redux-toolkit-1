2324f0f9d67bd7e484cab60ed0135fb0ee53e824
ozkostay <ozkostay@mail.ru>
2023-04-27 16:56:13 +0500

    start 270423

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MainApp2 from './components/MainApp';
+import PhoneMain from './components/PhoneMain';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <MainApp2 />
+      <br /><hr /><br /> */}
+      <PhoneMain />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/action/phoneVars.js
@@ -0,0 +1,22 @@
+export const SET_NAME = 'SET_NAME';
+export const SET_PRICE = 'SET_PRICE';
+export const SAVE_REPAIR = 'SAVE_REPAIR';
+export const CANSEL_REPAIR = 'CANSEL_REPAIR';
+export const EDIT_REPAIR = 'EDIT_REPAIR';
+export const DELETE_REPAIR = 'DELETE_REPAIR';
+export const INCREMENT_ID = 'INCREMENT_ID';
+export const EDIT_NOW = 'EDIT_NOW';
+
+
+const obj = {
+  SET_NAME,
+  SET_PRICE,
+  SAVE_REPAIR,
+  CANSEL_REPAIR,
+  EDIT_REPAIR,
+  DELETE_REPAIR,
+  INCREMENT_ID,
+  EDIT_NOW,
+};
+
+export default obj;
